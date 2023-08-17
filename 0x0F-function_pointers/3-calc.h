1bc231a53c392d98447a97dcb6fa40799ba3e415
warmflame <rashidasmoe@gmail.com>
2023-08-17 18:44:14 +0400

    3-calc.h update

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,7 +3,12 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 /**
  * struct op - Struct op
  *
@@ -15,13 +20,4 @@ typedef struct op
 char *op;
 int (*f)(int a, int b);
 } op_t;
-
-/* Function prototypes */
-int (*get_op_func(char *s))(int, int):
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
-#endif 
+#endif
