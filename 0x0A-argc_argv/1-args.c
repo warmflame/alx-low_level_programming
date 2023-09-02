68825a8345310ec5418700cac15ce068e711fcb4
warmflame <rashidasmoe@gmail.com>
2023-09-02 20:56:38 +0400

    added 1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the num of args passed into the program
+ * argc: num of arguments
+ * argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv; /*the void function allows argv to be ignored*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
