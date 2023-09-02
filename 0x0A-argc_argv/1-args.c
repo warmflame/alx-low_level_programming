fbc721acf1aeea85a6e7d9ba2944bd881799cdce
warmflame <rashidasmoe@gmail.com>
2023-09-02 21:00:35 +0400

    betty update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 /**
  * main - prints the num of args passed into the program
- * argc: num of arguments
- * argv: array of arguments
+ * @argc: num of arguments
+ * @argv: array of arguments
  *
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
 	(void) argv; /*the void function ignores argv*/
