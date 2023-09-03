7b3fc8e75eaaf5971c0491ce62b73da504dcdbc4
warmflame <rashidasmoe@gmail.com>
2023-09-04 02:09:15 +0400

    4-add created

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argc: num of arugments
+ * argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	if 
+}
