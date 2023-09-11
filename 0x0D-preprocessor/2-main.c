5b72837ab6f1ee44f1579e7722c83c5401beddc4
warmflame <rashidasmoe@gmail.com>
2023-09-11 21:38:15 +0400

    update void

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - a program that prints name of file it compiled from
  *
  * Return: 0
  */
-int main(void)
+int main()
 {
 	printf(("This program prints the file it was compiled from: %s\n", __FILE__);
 	return 0;
